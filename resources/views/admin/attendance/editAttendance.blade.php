1a26d6dc27b6a0bfeea2a3ccf44a164ffb865e88
Nour Hasan <nh_bappy@outlook.com>
2018-03-28 15:26:38 +0600

    bug fixed
    updating attendance view's bug fixed

--- resources/views/admin/attendance/editAttendance.blade.php
+++ resources/views/admin/attendance/editAttendance.blade.php
@@ -10,6 +10,15 @@
 
         @include('layouts.css')
         @include('layouts.js')
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<script>
+			$( function() {
+				$( "#date" ).datepicker({dateFormat: 'yy-mm-dd'});
+			} );
+		</script>
     </head>
 
     <body>
